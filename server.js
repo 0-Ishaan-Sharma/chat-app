9260bc2eda08979b8d8f111bb1227c3920edd81c
Ishaan Sharma <ishan.sharma464@gmail.com>
2026-01-09 12:35:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -70,7 +63,8 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
