df64802b6ec9dcf1f624099a36ed908dc1d6a60f
Ishaan Sharma <ishan.sharma464@gmail.com>
2026-01-12 12:19:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Store connected users
 const users = new Map();
